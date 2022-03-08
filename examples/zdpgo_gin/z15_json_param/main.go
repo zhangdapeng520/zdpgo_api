bb3b768ac9aff0fdf0e5c7e8fe13823b6974816e
zhangdapeng520 <pygosuperman@outlook.com>
2022-03-08 11:49:15 +0800

    版本1.0.4

--- examples/zdpgo_gin/z15_json_param/main.go
+++ examples/zdpgo_gin/z15_json_param/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/zhangdapeng520/zdpgo_gin"
 	"log"
 	"net/http"
 )
@@ -12,10 +13,13 @@ type Person struct {
 }
 
 func main() {
-	route := gin.Default()
+	// 创建核心对象
+	g := zdpgo_gin.New(zdpgo_gin.GinConfig{
+		Debug: true,
+	})
 
 	// 通过map获取json数据
-	route.POST("/", func(c *gin.Context) {
+	g.App.POST("/", func(c *gin.Context) {
 		json := make(map[string]interface{}) // 注意该结构接受的内容
 		c.BindJSON(&json)
 		log.Printf("%v", &json)
@@ -26,7 +30,7 @@ func main() {
 	})
 
 	// 通过struct获取json数据
-	route.PUT("/", func(c *gin.Context) {
+	g.App.PUT("/", func(c *gin.Context) {
 		json := Person{}
 		c.BindJSON(&json)
 		log.Printf("%v", &json)
@@ -37,5 +41,5 @@ func main() {
 	})
 
 	// { "name": "张大鹏" , "age": 22}
-	route.Run(":8080")
+	g.Run()
 }
