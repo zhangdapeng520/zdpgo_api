b20c7a008ac63a0f006ad8a5e796a875c085bbf9
张大鹏 <82651084+zhangdapeng520@users.noreply.github.com>
2022-05-05 10:59:30 +0800

    优化：根据日志创建gin对象

--- gin.go
+++ gin.go
@@ -5,9 +5,19 @@ import "github.com/zhangdapeng520/zdpgo_api/libs/gin"
 // NewGinWithLog 根据是否使用详细日志创建Gin的实例
 // @param isDetailLogger 是否使用详细日志，开启后，会记录请求头，form，body的详细信息
 func NewGinWithLog(isDetailLogger bool) *gin.Engine {
+	// 创建gin对象
 	r := gin.New()
 	r.Use(gin.LoggerWithConfig(gin.LoggerConfig{
-		IsDetailLogger: true, // 使用详细日志
+		IsDetailLogger: isDetailLogger, // 使用详细日志
 	}), gin.Recovery())
+
+	// 设置模式
+	if isDetailLogger {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	// 返回实例
 	return r
 }
